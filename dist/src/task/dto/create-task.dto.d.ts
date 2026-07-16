30a73a24f6046b63969b4d5e7c1ba0dda35adfab
JesseEmmanuel <jesseemmanuel@gmail.com>
2026-07-16 21:01:57 +0800

    chore: remove build and dist

--- dist/src/task/dto/create-task.dto.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class CreateTaskDto {
-    title: string;
-    description: string;
-    completed: boolean;
-    parentId?: string;
-    assigned_by_id: string;
-    assignee_id: string;
-}
